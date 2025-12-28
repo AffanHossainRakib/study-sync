1e28e259cd84ac1a901d2fad7b70ef11afe6e7d2
Md. Affan Hossain Rakib <affanhossainrakib@gmail.com>
2025-12-28 23:53:58 +0600

    feat: Add Vercel Analytics for improved tracking and insights

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer";
 import { AuthProvider } from "@/providers/AuthProvider";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -99,6 +100,7 @@ export default function RootLayout({ children }) {
             <Navbar />
             <main className="pt-16">{children}</main>
             <Footer />
+            <Analytics />
           </AuthProvider>
         </ThemeProvider>
       </body>
